a0206b807989a64e6fe01e05dbe309316c66b572
qeunasd <175177581+qeunasd@users.noreply.github.com>
2026-03-04 00:45:16 +0700

    feat: add option screen and button for each screen

--- src/main.c
+++ src/main.c
@@ -1,4 +1,3 @@
-// Standard include
 #include "mmapalloc.h"
 #include "raylib.h"
 #include <stdbool.h>
@@ -10,12 +9,37 @@
 #define GAMEEE_DEFAULT_FPS 60
 #define GAMEEE_MAX_CONTEXT_SWITCH 3
 
-// Contoh kasar
+typedef struct {
+    Rectangle bounds;
+    Color bgColor;
+    const char *text;
+    Color textColor;
+    int textSize;
+} Button;
 
-enum GameeeContextFlags { MainWindows, MenuWindows, None };
+bool IsButtonPressed(Button *btn) {
+    if (CheckCollisionPointRec(GetMousePosition(), btn->bounds) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+        return true;
+    }
+    return false;
+}
+
+void DrawButton(Button *btn) {
+    // Uncomment the code below to enable center text alignment
+    // and change X position parameter of the text in DrawText to textX
+    // int textWidth = MeasureText(btn->text, btn->textSize);
+    // int textX = btn->bounds.x + btn->bounds.width*0.5f - textWidth*0.5f;
+    int textY = btn->bounds.y + btn->bounds.height*0.5f - btn->textSize*0.5f;
+
+    DrawRectangleRec(btn->bounds, btn->bgColor);
+    DrawText(btn->text, btn->bounds.x + 20.0f, textY, btn->textSize, btn->textColor);
+}
+
+enum GameeeContextFlags { MainWindows, MenuWindows, OptionWindows, None };
 
 struct GameeeContextMetadata {
     bool windowsLifetime;
+    enum GameeeContextFlags windowPrev;
 };
 
 struct GameeeContextAssets {
@@ -40,6 +64,28 @@ enum GameeeContextFlags MainWin(struct GameeeContextMetadata *metadata,
 
 #endif /* ifdef __WIN_32  #include <windows.h> __declspec(dllexport) */
 
+    Button btnResume = {
+        .bounds = (Rectangle){ 100.0f, 200.0f, 200.0f, 50.0f},
+        .bgColor = DARKGRAY,
+        .text = "RESUME",
+        .textColor = WHITE,
+        .textSize = 30,
+    };
+    Button btnOption = {
+        .bounds = (Rectangle){ 100.0f, 270.0f, 200.0f, 50.0f},
+        .bgColor = DARKGRAY,
+        .text = "OPTION",
+        .textColor = WHITE,
+        .textSize = 30,
+    };
+    Button btnReturn = {
+        .bounds = (Rectangle){ 100.0f, 340.0f, 200.0f, 50.0f},
+        .bgColor = DARKGRAY,
+        .text = "RETURN",
+        .textColor = WHITE,
+        .textSize = 30,
+    };
+
     Vector2 *vec = &shared[MainWindows].vec;
     // event handler
     if (IsKeyPressed(KEY_A)) {
@@ -48,9 +94,21 @@ enum GameeeContextFlags MainWin(struct GameeeContextMetadata *metadata,
     if (IsKeyDown(KEY_SPACE)) {
         vec->y += 10;
     }
-    ClearBackground(GREEN);
-    DrawText("INFO : hello from main windows", 100, 100, 20, WHITE);
-    DrawRectangle(100, 100, (int)vec->x, (int)vec->y, BLACK);
+
+    if (IsButtonPressed(&btnResume)) return MainWindows;
+    if (IsButtonPressed(&btnOption)) {
+        metadata->windowPrev = MainWindows;
+        return OptionWindows;
+    }
+    if (IsButtonPressed(&btnReturn)) return MenuWindows;
+
+    ClearBackground(BLACK);
+    DrawText("MAIN WINDOWS", 100, 100, 50, WHITE);
+    DrawRectangle(50, 100, (int)vec->x, (int)vec->y, LIME);
+    DrawButton(&btnResume);
+    DrawButton(&btnOption);
+    DrawButton(&btnReturn);
+
     return MainWindows;
 }
 
@@ -58,6 +116,35 @@ enum GameeeContextFlags MainWin(struct GameeeContextMetadata *metadata,
 
 enum GameeeContextFlags MenuWin(struct GameeeContextMetadata *metadata,
                                 struct GameeeContextAssets *shared) {
+    Button btnNewGame = {
+        .bounds = (Rectangle){ 100.0f, 200.0f, 200.0f, 50.0f},
+        .bgColor = DARKGRAY,
+        .text = "NEW GAME",
+        .textColor = WHITE,
+        .textSize = 30,
+    };
+    Button btnContinue = {
+        .bounds = (Rectangle){ 100.0f, 270.0f, 200.0f, 50.0f},
+        .bgColor = DARKGRAY,
+        .text = "CONTINUE",
+        .textColor = WHITE,
+        .textSize = 30,
+    };
+    Button btnOption = {
+        .bounds = (Rectangle){ 100.0f, 340.0f, 200.0f, 50.0f},
+        .bgColor = DARKGRAY,
+        .text = "OPTION",
+        .textColor = WHITE,
+        .textSize = 30,
+    };
+    Button btnExit = {
+        .bounds = (Rectangle){ 100.0f, 410.0f, 200.0f, 50.0f},
+        .bgColor = DARKGRAY,
+        .text = "EXIT",
+        .textColor = WHITE,
+        .textSize = 30,
+    };
+
     Vector2 *vec = &shared[MenuWindows].vec;
     if (IsKeyPressed(KEY_A)) {
         return MainWindows;
@@ -65,11 +152,52 @@ enum GameeeContextFlags MenuWin(struct GameeeContextMetadata *metadata,
     if (IsKeyDown(KEY_SPACE)) {
         vec->y += 10;
     }
-    ClearBackground(BLUE);
-    DrawText("INFO : hello from menu windows", 100, 200, 20, WHITE);
-    DrawRectangle(100, 100, (int)vec->x, (int)vec->y, BLACK);
-    return None;
+    if (IsButtonPressed(&btnNewGame)) return MainWindows;
+    if (IsButtonPressed(&btnContinue)) return MainWindows;
+    if (IsButtonPressed(&btnOption)) {
+        metadata->windowPrev = MenuWindows;
+        return OptionWindows;
+    }
+    if (IsButtonPressed(&btnExit)) CloseWindow();
+
+    ClearBackground(BLACK);
+    DrawText("MENU WINDOWS", 100, 100, 50, WHITE);
+    DrawRectangle(50, 100, (int)vec->x, (int)vec->y, SKYBLUE);
+    DrawButton(&btnNewGame);
+    DrawButton(&btnContinue);
+    DrawButton(&btnOption);
+    DrawButton(&btnExit);
+
+    return MenuWindows;
 }
+
+// Option State
+
+enum GameeeContextFlags OptionWin(struct GameeeContextMetadata *metadata,
+                                struct GameeeContextAssets *shared) {
+    Button btnReturn = {
+        .bounds = (Rectangle){ 100.0f, 200.0f, 200.0f, 50.0f},
+        .bgColor = DARKGRAY,
+        .text = "RETURN",
+        .textColor = WHITE,
+        .textSize = 30,
+    };
+
+    if (IsKeyPressed(KEY_A)) {
+        return MainWindows;
+    }
+    if (IsButtonPressed(&btnReturn)) {
+        if (metadata->windowPrev == MainWindows) return MainWindows;
+        if (metadata->windowPrev == MenuWindows) return MenuWindows;
+    }
+
+    ClearBackground(BLACK);
+    DrawText("OPTION WINDOWS", 100, 100, 50, WHITE);
+    DrawButton(&btnReturn);
+
+    return OptionWindows;
+}
+
 
 int main(void) {
     const int screenWidth = 1920, screenHeight = 1200, minScreenWidth = 810,
@@ -97,9 +225,10 @@ int main(void) {
         return EXIT_FAILURE;
     }
     mdata.windowsLifetime = true;
+    mdata.windowPrev = None;
 
     context_assets[MainWindows].vec = (Vector2){10, 20};
-    context_assets[MenuWindows].vec = (Vector2){100, 100};
+    context_assets[MenuWindows].vec = (Vector2){10, 20};
 
     struct GameeeContextSwitching switchCtx;
     /*
@@ -108,8 +237,9 @@ int main(void) {
 
     switchCtx.switchWindows[MainWindows] = MainWin;
     switchCtx.switchWindows[MenuWindows] = MenuWin;
+    switchCtx.switchWindows[OptionWindows] = OptionWin;
 
-    enum GameeeContextFlags flagsCtx = MainWindows;
+    enum GameeeContextFlags flagsCtx = MenuWindows;
 
     while (!WindowShouldClose()) {
         BeginDrawing();
@@ -119,6 +249,9 @@ int main(void) {
             break;
         case MenuWindows:
             flagsCtx = MenuWindows;
+            break;
+        case OptionWindows:
+            flagsCtx = OptionWindows;
             break;
         case None:
             // fprintf(stderr,"ERROR : failed to change state,state not
